ab4200762ce56d399d2873464d44de498cec1284
Luiskar <luiskar009@gmail.com>
2017-03-04 13:10:17 +0100

    Function updateInventryTable added

--- UpdateStock/UpdateStock/Program.cs
+++ UpdateStock/UpdateStock/Program.cs
@@ -7,6 +7,7 @@ using System.Data.SqlClient;
 using MySql.Data;
 using MySql.Data.MySqlClient;
 using System.Configuration;
+using System.Data;
 
 
 
@@ -30,6 +31,12 @@ namespace UpdateStock
             createList(connStringMySql);
         }
 
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///                                                                                                                   ///
+        ///                             Create a List with the articules to update the stock                                  ///
+        ///                                                                                                                   ///
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         public static List<String> createList(String connStringMySql)
         {
             List<String> list = new List<string>();
@@ -46,7 +53,41 @@ namespace UpdateStock
                 }
             }
             return list;
-        }
+        }//createList
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        ///                                                                                                                   ///
+        ///                                 Update table InventarioTabla with stock                                           ///
+        ///                                                                                                                   ///
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        public static void updateInventoryTable(String connStringMySql, String connStringSqlServer, List<String> list)
+        {
+            using (SqlConnection conn = new SqlConnection(connStringSqlServer))
+            {
+                foreach (String element in list)
+                {
+                    SqlCommand cmd = new SqlCommand($"SELECT UnidadesStock FROM ocartacp WHERE Articulo = '{element}'", conn);
+                    if (conn.State == ConnectionState.Closed)
+                        conn.Open();
+                    using (SqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        rdr.Read();
+                        String Stock = rdr[0].ToString();
+                        using (MySqlConnection conn2 = new MySqlConnection(connStringMySql))
+                        {
+                            MySqlCommand cmd2 = new MySqlCommand($"UPDATE InventarioTablas SET Stock = '{Stock}' WHERE Articulo = '{element}';", conn2);
+                            MySqlCommand cmd3 = new MySqlCommand($"UPDATE InventarioTablas SET xxxFechaHoraActualizacion = now() WHERE Articulo = '{element}';", conn2);
+                            if (conn2.State == ConnectionState.Closed)
+                                conn2.Open();
+                            cmd2.ExecuteNonQuery();
+                            cmd3.ExecuteNonQuery();
+                        }
+                    }
+                }
+            }
+        }//updateInventoryTable
+
 
     }
 }
